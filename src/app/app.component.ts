7a5ae38e63f53abc45791819a585800b8ec6f30b
Team Neugeschaeft <gpk-d-it-kuv-fonl2@allianz.de>
2018-07-03 12:05:04 +0200

    First version is ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, map, startWith, takeWhile } from 'rxjs/operators';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit, OnDestroy {
+  @ViewChild('testLink') testLink: ElementRef;
+  public mailtoForm = new FormGroup({
+    recipient: new FormControl('', Validators.email),
+    subject: new FormControl(''),
+    mailbody: new FormControl('')
+  });
+
+  private alive = true;
+
+  public mailtoUrl$: Observable<string>;
+
+  constructor(private snackBar: MatSnackBar) { }
+
+  ngOnInit() {
+    this.mailtoUrl$ = this.mailtoForm.valueChanges.pipe(
+      filter(() => this.mailtoForm.valid),
+      debounceTime(200),
+      distinctUntilChanged(),
+      map(this.tranformToUrl),
+      startWith('mailto:'),
+      takeWhile(() => this.alive)
+    );
+  }
+
+  ngOnDestroy() {
+    this.alive = false;
+  }
+
+  public onTestUrl(): void {
+    this.testLink.nativeElement.click();
+  }
+
+  public onCopy(target: HTMLTextAreaElement): void {
+    target.select();
+    document.execCommand('copy');
+    this.snackBar.open('Text has been copied to clipboard', 'CLOSE', {
+      duration: 2000
+    });
+  }
+
+  private tranformToUrl({recipient, subject, mailbody}: {recipient: string, subject: string, mailbody: string}): string {
+    const subjectPart = subject ? `subject=${subject}` : '';
+    const mailbodyPart = mailbody ? `body=${mailbody}` : '';
+    const queryPart = recipient && subjectPart || mailbodyPart ? '?' : '';
+    const concatPart = subject && mailbodyPart ? '&' : '';
+    return encodeURI(`mailto:${recipient}${queryPart}${subjectPart}${concatPart}${mailbodyPart}`);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCardModule, MatInputModule, MatSnackBarModule, MatToolbarModule } from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
